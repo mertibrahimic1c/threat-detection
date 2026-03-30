2e949376a88f869e7378e86c926ef368ab0bc9c3
İbrahim Mert Çiçek <mertibrahimic1c@gmail.com>
2026-03-30 22:53:18 +0300

    fix: kod genelindeki mantık hatası giderildi

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import cv2 #Video okuma, kutu çizme, ekranda gösterme
-import json #Veriyi dosyaya kaydetmek ve okumak
-import os #.env dosyasını okur, dosya var mı yok mu kontrol eder
-import time #API için time sınırlama 15*4=60 
-from dotenv import load_dotenv #.env dosyasındaki key'i programa yükler
-import google.generativeai as genai #gemini API'i bağlar
-from ultralytics import YOLO #nesneyi tanır
+import cv2 
+import json 
+import os 
+import time 
+from dotenv import load_dotenv 
+import google.generativeai as genai 
+from ultralytics import YOLO 
 
-#.env dosyasını yükler
+
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
@@ -40,36 +40,36 @@ def  ask_gemini(name):
         return "safe"
         
 
-cap=cv2.VideoCapture("demo.mp4") #görüntüyü alır
+cap=cv2.VideoCapture("demo.mp4") 
 
 
 while True:
-    ret, frame=cap.read() #piksel  piksel alır. Başaralı ise ret true değilse false.
+    ret, frame=cap.read() 
 
     if not ret:
         print("Unable to play video")
         break
     
-    results=model_yolo(frame) #tespit edilen nesneleri döndürür
-    detections=results[0].boxes #bıçak, kaşık, kitap
+    results=model_yolo(frame) 
+    detections=results[0].boxes 
 
-    for box  in detections: #nesneleri tek tek alır
-        class_id=int(box.cls[0]) #nesnenin sınıf numarasını döndürür
-        name=model_yolo.names[class_id] #numarayı nesnenin ismine çevirir
+    for box  in detections: 
+        class_id=int(box.cls[0]) 
+        name=model_yolo.names[class_id] 
 
-        if name not in cache: #daha önceden nesne tanımlanmadıysa
-            cache[name]=ask_gemini(name) #cache["scissors"] = "dangerous" gemini'ye sorar ve cache içine yazar
-            with open("cache.json","w") as f: #kalıcı cahce içine yazar
+        if name not in cache: 
+            cache[name]=ask_gemini(name) 
+            with open("cache.json","w") as f: 
                 json.dump(cache,f)
             time.sleep(4)
         
-        decision=cache[name] #nesnenin dangerous mu yoksa safe mi döndürür
+        decision=cache[name] 
         if decision == "dangerous": 
-            color=(0,0,255) #kırmızı yapar
+            color=(0,0,255) 
         else:
-            color=(0,225,0) #yeşil yapar
+            color=(0,225,0) 
 
-        x1, y1, x2, y2=map(int,box.xyxy[0]) #nesnenin kordinatlarını alır   
+        x1, y1, x2, y2=map(int,box.xyxy[0])   
         cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
         
         cv2.putText(frame, f"{name} - {decision}",(x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
